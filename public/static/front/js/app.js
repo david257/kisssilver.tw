34a1627f2b8305a3b335b3b5ba0a30da14d335c5
Joe <joecheng91@hotmail.com>
2022-01-11 03:42:06 +0800

    test img lazy loading

--- public/static/front/js/app.js
+++ public/static/front/js/app.js
@@ -508,20 +508,23 @@ $(".hot-owl .owl-carousel .item video").css({"width" : hotowl1});
 			}
 	});
 
-	function onEnterView(entries, observer) {
-		for (let entry of entries) {
-			if (entry.isIntersecting) {
-				// 監視目標進入畫面
-				const img = entry.target
-				img.setAttribute('src', img.dataset.src) // 把值塞回 src
-				img.removeAttribute('data-src')
-				observer.unobserve(img) // 取消監視
-			}
+function onEnterView(entries, observer) {
+	for (let entry of entries) {
+		if (entry.isIntersecting) {
+			// 監視目標進入畫面
+			const img = entry.target
+			img.setAttribute('src', img.dataset.src) // 把值塞回 src
+			img.removeAttribute('data-src')
+			observer.unobserve(img) // 取消監視
 		}
 	}
+}
 
-	const watcher = new IntersectionObserver(onEnterView)
-	const lazyImages = document.querySelectorAll('img.lazy')
-	for (let image of lazyImages) {
-		watcher.observe(image) // 開始監視
-	}
+const watcher = new IntersectionObserver(onEnterView)
+const lazyImages = document.querySelectorAll('img.lazy')
+for (let image of lazyImages) {
+	watcher.observe(image) // 開始監視
+}
+window.fadeIn = function(obj) {
+	$(obj).fadeIn(1500);
+}
