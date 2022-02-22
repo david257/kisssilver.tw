f6da023390915cfe1f84f51b8d86801e2a9e565e
david.zhang <david.zhang257@qq.com>
2022-02-22 15:07:46 +0800

    1

--- public/static/front/js/app.js
+++ public/static/front/js/app.js
@@ -5,14 +5,20 @@ $(function(){
 	var newsproducw=$(".news-product .list-one").width();
 $(".news-product .list-one .list-pic img").css({"width" : newsproducw});
 $(".news-product .list-one .list-pic video").css({"width" : newsproducw});
+$(".news-product .list-one .list-pic img").css({"height" : newsproducw*7/9});
+$(".news-product .list-one .list-pic video").css({"height" : newsproducw*7/9});
 
 var hotproductw=$(".hot-product .list-one").width();
 $(".hot-product .list-one .list-pic img").css({"width" : hotproductw});
 $(".hot-product .list-one .list-pic video").css({"width" : hotproductw});
+$(".hot-product .list-one .list-pic img").css({"height" : hotproductw*7/9});
+$(".hot-product .list-one .list-pic video").css({"height" : hotproductw*7/9});
 
 var saleproductw=$(".sale-product .list-one").width();
 $(".sale-product .list-one .list-pic img").css({"width" : saleproductw});
 $(".sale-product .list-one .list-pic video").css({"width" : saleproductw});
+$(".sale-product .list-one .list-pic img").css({"height" : saleproductw*7/9});
+$(".sale-product .list-one .list-pic video").css({"height" : saleproductw*7/9});
 		if(bodyw > 768){
 			var locationbg=$(".hide-xs .location-bg").width()*600 / 1920;
 		$(".hide-xs .location-bg").css({"height" : locationbg});
