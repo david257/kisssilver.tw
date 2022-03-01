9bc6c7042f80fcafc63e0470c0fcde775c26a21f
Joe <joecheng91@hotmail.com>
2022-03-01 22:57:39 +0800

    fix banner cdn

--- app/view/index/index.php
+++ app/view/index/index.php
@@ -7,6 +7,17 @@
     {include file="public/header" /}
     <?php if(!empty($header_banners)) { ?>
     <section class="banner">
+    <?php foreach ($header_banners as $banner) { ?>
+        <a target="_blank" href="{:$banner['url']}">
+                    <?php
+                        if(strpos($banner['imagefile'], '.mp4') !== false) {
+                        ?>
+                            <video controls><source src="{:showfile($banner['imagefile'])}" data-src="{:showfile($banner['imagefile'])}" data-small="{:showfile($banner['min_imagefile'])}"  data-medium="{:showfile($banner['min_imagefile'])}" data-large="{:showfile($banner['min_imagefile'])}" type="video/mp4"></video>
+                        <?php } else { ?>
+                    	<img alt="{:$banner['title']}" class="sp-image" src="/static/front/images/logo.png" data-src="{:showfile($banner['imagefile'])}" data-small="{:showfile($banner['min_imagefile'])}"  data-medium="{:showfile($banner['min_imagefile'])}" data-large="{:showfile($banner['min_imagefile'])}"/>
+                    <?php } ?>
+        </a>
+    <?php } ?>
         <div id="index-slider" class="slider-pro index-banner">
             <div class="sp-slides">
                 <?php foreach ($header_banners as $banner) { ?>
