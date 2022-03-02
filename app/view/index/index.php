106328f50738609292893e7b93868e17c4a25cc0
Joe <joecheng91@hotmail.com>
2022-03-03 01:11:34 +0800

    modify carousel

--- app/view/index/index.php
+++ app/view/index/index.php
@@ -24,7 +24,7 @@
     <?php } ?>
     <?php if(!empty($header_three_banners)) { ?>
     <section class="index-three-gg">
-          <div class="owl-carousel show-md">
+          <div class="owl-carousel show-xs">
               <?php foreach ($header_three_banners as $banner) { ?>
               <div class="item">
                   <?php
