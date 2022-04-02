430f8e48584186bff1f5d828df124ec7d0126fbe
Joe <joecheng91@hotmail.com>
2022-04-02 18:20:22 +0800

    modify scroll to top

--- app/view/public/kefu.php
+++ app/view/public/kefu.php
@@ -26,7 +26,6 @@
             <use xlink:href="#icon-ins"></use>
         </svg>
     </a>
-    <a rel="noreferrer" class="toolbox__button toolbox__button--shadow" target="_blank" style="font-size: 40px;"><i class="toolbox__icon icon icon-top"></i></a>
     <!-- <a href="javascript:void(0)" id="CheckoutBtn" class="btn btn-default no-margin pull-right col-xs-12 col-md-3">
         <i class="fa fa-long-arrow-up"></i>
     </a> -->
